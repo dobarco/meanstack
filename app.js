3eb844ca175148b219bc3d91fbb1746c676e66bd
dobarco <rafand@rafaels-mbp.br.ibm.com>
2017-07-25 15:37:24 -0300

    Configuring Authentication for Back-end

--- app.js
+++ app.js
@@ -4,16 +4,21 @@ const cors = require('cors');
 const bodyParser=require('body-parser');
 const config=require('./config/database');
 const mongoose=require('mongoose');
+const passport=require('passport');
+
 
 //Connect to Database
 mongoose.connect(config.database);
 
+//Sucess
 mongoose.connection.on('connected',function(){
 	console.log("Connected to the database: "+config.database);
 })
+//Error
 mongoose.connection.on('error',function(err){
 	console.log("Database error: "+config.database+err);
 })
+//Declaring Express variable
 const app=express();
 
 //Port Number
@@ -21,8 +26,13 @@ const port=3000;
 
 const users=require('./routes/users');
 
+//Body Parser Middleware 
 app.use(bodyParser.json());
 
+//Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+require('./config/passport')(passport);
 app.use(express.static(path.join(__dirname,'public')));
 
 //Index Route
